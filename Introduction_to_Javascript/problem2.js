7ab163d9be9e0ffcd80059d6d10f5c2cde1f114a
pooooh14 <monisha1497@gmail.com>
2022-12-27 03:10:29 +0530

    day 1 programs

---
+++ Introduction_to_Javascript/problem2.js
@@ -0,0 +1,8 @@
+var name = "Monisha Verma";
+console.log(name);
+
+name = "Mr. Mahesh Kumar Verma";
+console.log(name);
+
+name = "Mrs. Madhuri Verma";
+console.log(name);
---
+++ Introduction_to_Javascript/problem3.js
@@ -0,0 +1,6 @@
+var name = "Monisha Verma";
+var age = 25;
+
+console.log(name, age);
+console.log(typeof(name));
+console.log(typeof(age));
